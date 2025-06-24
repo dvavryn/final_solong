0f2bbd8f0b110800bc756a063d7154d26c578022
dvavryn <vavryn.dominic@outlook.com>
2025-06-25 01:05:10 +0200

    almost fin

--- copy_split.c
+++ copy_split.c
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 00:51:42 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/06/25 00:57:20 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/06/25 01:03:38 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,7 @@ char	**copy_split(char **split)
 		;
 	out = malloc(i * sizeof(char *));
 	if (!out)
-	{
-		free_split(split);
-		exit(1);
-	}
+		return (NULL);
 	out[i - 1] = NULL;
 	i = -1;
 	while (split[++i])
